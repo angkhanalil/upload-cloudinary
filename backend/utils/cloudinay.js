1d676bb5bc0de148efd5f4661cb738d30b64dd4a
Angkhana <angkhana.lil94@gmail.com>
2023-06-18 14:28:43 +0700

    fix

--- backend/utils/cloudinay.js
+++ backend/utils/cloudinay.js
@@ -6,7 +6,7 @@ const cloudinary = cloudinaryModule.v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  api_secret: process.env.API_SECRET_KEY,
   secure: true,
 });
 
